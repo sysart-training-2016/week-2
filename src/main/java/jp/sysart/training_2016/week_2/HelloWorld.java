483abf88a85769534a75dda3f827200c459d8b48
Kenichi Masuko <kenichi@dhcp-210.shibuya.sysart.jp>
2016-03-11 14:37:26 +0900

    Hello Worlds

---
+++ src/main/java/jp/sysart/training_2016/week_2/HelloWorld.java
@@ -0,0 +1,14 @@
+package jp.sysart.training_2016.week_2;
+
+/**
+ * コンソールに文字列を出力する。
+ */
+public class HelloWorld {
+	/**
+	 * プログラムの開始メソッド。
+	 * @param args コマンドライン引数。
+	 */
+	public static void main(String[] args) {
+		System.out.println("こんにちは世界");
+	}
+}
---
+++ src/main/scala/jp.sysart.training_2016.week_2/HelloWorld2.scala
@@ -0,0 +1,5 @@
+package jp.sysart.training_2016.week_2
+
+object HelloWorld2 extends App {
+	println("こんにちは世界 2")
+}
